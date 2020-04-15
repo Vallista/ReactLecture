ffca2829e36a35507752eb93ac01e088f3785406
마광휘 <mgh950714@gmail.com>
2020-04-15 10:31:50 +0900

    에디터 구현

--- src/components/InputTwit/index.jsx
+++ src/components/InputTwit/index.jsx
@@ -6,7 +6,67 @@ import Button from '../Button'
 import styles from './index.module.scss'
 
 const InputTwit = ({ profile, onTweet }) => {
-  const [ textarea, setTextarea ] = useState('')
+  // const [ textarea, setTextarea ] = useState('')
+
+  const Editor = ({}) => {
+    const [ textList, setTextList ] = useState([ '' ])
+
+    const onEnter = (e, index) => {
+      if (e.key === 'Backspace' && textList[index] === '') {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const tempList = textList.slice()
+        tempList.splice(index, 1)
+        setTextList(tempList)
+
+        return
+      }
+
+      if (e.key === 'Enter' && e.shiftKey) {
+        // e.preventDefault = 이벤트를 무시하겠다. = 실제로 텍스트에 적용하지 않겠다
+        e.preventDefault()
+        // e.stopPropagation = 이벤트 전파를 중지하겠다. = 상위의 이벤트에 이벤트전달을 하지 않겠다.
+        e.stopPropagation()
+
+        // console.log('개행!')
+        const tempList = textList.slice()
+        tempList.push('')
+        setTextList(tempList)
+
+        return
+      }
+
+      const tempList = textList.slice()
+      tempList[index] = e.target.innerText
+      setTextList(tempList)
+      // textList.slice() <- 배열이 복사됨
+
+      // e = keyCode
+      // const code = e.keyCode // keyCode
+      // console.log('In decimal ' + e.keyCode) // keyCode = 10
+
+      // const hex = code.toString(16)
+      // console.log('In hex ' + hex) // 0x00 = 16진수
+    }
+
+    return (
+      <div className={styles.editorWrapper}>
+        {/* onKeyDown = 눌렀을 때 최초 1회 */}
+        {textList.map((text, index) => (
+          <div
+            key={index}
+            value={text}
+            contentEditable='true'
+            className={styles.editor}
+            onKeyDown={(e) => {
+              onEnter(e, index)
+            }}
+          />
+        ))}
+      </div>
+    )
+  }
 
   return (
     <div className={styles.default}>
@@ -16,14 +76,15 @@ const InputTwit = ({ profile, onTweet }) => {
 
       <div className={styles.textBox}>
         <div className={styles.inputBox}>
-          <textarea
+          {/* <textarea
             value={textarea}
             onChange={(e) => {
               setTextarea(e.currentTarget.value)
             }}
             className={styles.textarea}
             placeholder='무슨 일이 일어나고 있나요?'
-          />
+          /> */}
+          <Editor />
         </div>
 
         <div className={styles.optionals}>
@@ -40,7 +101,7 @@ const InputTwit = ({ profile, onTweet }) => {
             </div>
             <Button
               onClick={() => {
-                onTweet(textarea)
+                onTweet(textList.toString())
               }}
               type='button'
               className={styles.twitButton}
--- src/components/TweetList/index.jsx
+++ src/components/TweetList/index.jsx
@@ -1,31 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './index.module.scss'
 
 const TweetList = ({ list }) => {
-  const Tweet = ({ profile, name, id, description, date, likeCount }) => (
-    <div className={styles.tweet}>
-      <div className={styles.profile}>
-        <img src={profile} alt='profile' />
-      </div>
-      <div className={styles.contents}>
-        <div className={styles.header}>
-          <span>{name}</span>
-          <span>{id}</span>
-          <span>{date}</span>
-        </div>
-        <p className={styles.description}>{description}</p>
-        <div className={styles.footer}>
-          <img src={null} alt='profile' />
-          <img src={null} alt='profile' />
-          <img src={null} alt='profile' />
-          <img src={null} alt='profile' />
-          <img src={null} alt='profile' />
-        </div>
-      </div>
+  const TweetOptionWindow = ({}) => (
+    <div className={styles.optionWindow}>
+      <ul className={styles.optionWindowList}>
+        <li className={styles.optionWindowListItem}>
+          <img />
+          <span>삭제</span>
+        </li>
+        <li className={styles.optionWindowListItem}>
+          <img />
+          <span>내 프로필 메인에 올리기</span>
+        </li>
+        <li className={styles.optionWindowListItem}>
+          <img />
+          <span>트윗 담아가기</span>
+        </li>
+        <li className={styles.optionWindowListItem}>
+          <img />
+          <span>트윗 활동 보기</span>
+        </li>
+      </ul>
     </div>
   )
+
+  const Tweet = ({ profile, name, id, description, date, likeCount }) => {
+    const [ isOptionOpen, setOptionOpen ] = useState(false)
+
+    // onFocus, onBlur
+    // onFocus: 이 친구가 선택이 되었을때,
+    // onBlur: 이 친구의 선택이 풀렸을때
+    return (
+      <div
+        className={styles.tweet}
+        onBlur={() => {
+          setOptionOpen(false)
+        }}
+      >
+        <div className={styles.profile}>
+          <img src={profile} alt='profile' />
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.header}>
+            <div>
+              <span>{name}</span>
+              <span>{id}</span>
+              <span>{date}</span>
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  setOptionOpen(!isOptionOpen)
+                }}
+                type='button'
+              >
+                버튼
+              </button>
+              {isOptionOpen && <TweetOptionWindow />}
+            </div>
+          </div>
+          <p className={styles.description}>{description}</p>
+          <div className={styles.footer}>
+            <img src={null} alt='profile' />
+            <img src={null} alt='profile' />
+            <img src={null} alt='profile' />
+            <img src={null} alt='profile' />
+            <img src={null} alt='profile' />
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return <div className={styles.tweetList}>{list.map((tweet) => <Tweet {...tweet} />)}</div>
 }
